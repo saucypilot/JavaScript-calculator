8d8d913b4139a3d414a70830ba28114c633ede9c
Irian Durian <durianirian@gmail.com>
2022-11-16 21:49:10 -0600

    Fixed

--- calculator.js
+++ calculator.js
@@ -89,7 +89,7 @@ class Calculator {
   }
 }
 
-const numberButtons = document.querySelectorAll("[data-numbers]");
+const numberButtons = document.querySelectorAll("[data-number]");
 const operationButtons = document.querySelectorAll("[data-operation]");
 const equalsButton = document.querySelector("[data-equals]");
 const deleteButton = document.querySelector("[data-delete]");
